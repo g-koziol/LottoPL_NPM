185f46f33ab4c292af8bc3a6321a3d516db260ec
paindg93 <gkoziol@wasko.pl>
2017-04-27 11:28:03 +0200

    Working reading Lotto game results from any date
    Finally figured out how to call many times async operations for getting
    info from Lotto.pl API

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 "use strict";
 
-
-// var appjs = require('appjs'); var http = require('http'); var XMLHttpRequest
-// = require("xmlhttprequest").XMLHttpRequest;
 var LottoSOAP = require('./lib/lottoSOAP');
 
 var lala = new LottoSOAP();
@@ -12,22 +9,14 @@ var lala = new LottoSOAP();
 // var error = (response) => {     this.result = 'error' };
 // lala.sendEnvelope(methodName, data, success, error);
 
-lala.getLastWynikiLotto("20170101", (err, result) => {
-  if (err)
-    console.log('Something bad happened');
-  result
-    .WynikLotto
-    .forEach(function (element) {
-var output = 'Data losowania: ' + element.data_losowania.$value + '\nNum losowania ' + element.num_losowania.$value + '\nNumerki: ' + element
-  .numerki
-  .$value
-  .split(',')
-  .join(" ");
-  console.log(output);
-    }, this);
+var data = [];
+var callback = function(data){
+  console.log(data);
   process.exit();
-});
+}
 
-async.parallel()
+lala.getAllWynikiLottoFromDate("01-01-2017",data,callback);
+
+//async.parallel()
 
 //process.exit();
